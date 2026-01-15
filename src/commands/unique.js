6c562aef3fe1bf53c942e1786085364974e7a5db
Michael <dxntlxxkbxck@gmail.com>
2026-01-15 13:07:43 +0300

    unique command

--- src/commands/unique.js
+++ src/commands/unique.js
@@ -1,1 +1,19 @@
+const unique = (recipe) => {
+    const result = [];
+    for (let i = 0; i < recipe.length; i++) {
+        const item = recipe[i];
+        let found = false;
+        for (let j = 0; j < result.length; j ++) {
+            if (result[j] === item) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result.push(item);
+        }
+    }
+    return result;
+}
 
+module.exports = unique;
