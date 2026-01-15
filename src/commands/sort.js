e964bf4e3c4bbc5729947c0c28cc5e0e31bf4c65
Michael <dxntlxxkbxck@gmail.com>
2026-01-15 13:10:31 +0300

    sort command

--- src/commands/sort.js
+++ src/commands/sort.js
@@ -1,1 +1,10 @@
+const sortRecipe = (recipe, direction = 'asc') => {
+    const copy = recipe.slice();
+    copy.sort((a, b) => a.localeCompare(b));
+    if (direction === 'desc') {
+        copy.reverse();
+    }
+    return copy;
+}
 
+module.exports = sortRecipe;
