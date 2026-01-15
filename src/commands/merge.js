c72f4e6a7b22e6851095e9f5bf8f0359883801ee
Michael <dxntlxxkbxck@gmail.com>
2026-01-15 13:13:49 +0300

    merge command

--- src/commands/merge.js
+++ src/commands/merge.js
@@ -1,1 +1,14 @@
+const merge = (recipe, csvList) => {
+    if (!csvList || typeof csvList !== 'string') {
+        return recipe.slice();
+    }
 
+    const parsed = csvList
+        .split(',')
+        .map((s) => s.trim())
+        .filter((s) => s.length > 0);
+
+    return [...recipe, ...parsed];
+}
+
+module.exports = merge;
