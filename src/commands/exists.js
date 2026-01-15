cf92f6e0ff74a9302bbd34032f9cbeb929dbdc31
Michael <dxntlxxkbxck@gmail.com>
2026-01-15 12:58:38 +0300

    exists command

--- src/commands/exists.js
+++ src/commands/exists.js
@@ -1,1 +1,8 @@
-
+const exists = (recipe, ingredient) => {
+    for (let i = 0; i < recipe.length; i++) {
+        if (recipe[i] === ingredient) {
+            return true;
+        }
+    }
+    return false;
+}
